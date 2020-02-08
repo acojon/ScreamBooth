af924eed27a487eb861cb93d874faa3eac4fc368
Jon <acojon@gmail.com>
2020-02-08 14:42:05 -0700

    Add some commenting

--- ScreamBooth/code.py
+++ ScreamBooth/code.py
@@ -5,9 +5,11 @@ import board
 
 soundVolume = AnalogIn(board.A0)
 triggerLevel = AnalogIn(board.A4)
+
 camera = DigitalInOut(board.A3)
 camera.direction = Direction.OUTPUT
 camera.value = False
+
 triggeredLed = DigitalInOut(board.A2)
 triggeredLed.direction = Direction.OUTPUT
 triggeredLed.value = False
@@ -23,18 +25,33 @@ blink_interval = .1
 while True:
     print("soundVolume:", soundVolume.value, "triggerLevel:", triggerLevel.value, "db_filter:", db_filter)
 
+    # If the current sound level is greater than the trigger level, one of two
+    # things has occurred:
+    #
+    # 1) The deadband filter is running, and the audio is still too loud to
+    #    allow the scream booth to trigger again
+    #
+    # 2) The deadband filter is not running, and the scream booth should take a
+    #    picture.
     if soundVolume.value >= triggerLevel.value:
         if db_filter:
+            # It's still too loud in the room, reset the timer.
             db_start_time = time.monotonic()
         else:
+            # Take a picture!
             camera.value = True
             triggeredLed.value = True
             time.sleep(.5)
             camera.value = False
+
+            # Turn on the dead band filtering.
             blink_led = True
             db_start_time = time.monotonic()
             db_filter = True
 
+    # If the deadband filter is active, there needs to be a way to exist the
+    # filter and start responding to sound again.  Check to see if enough time
+    # has passed that we can turn off the deadband filter. 
     if db_filter:
         now = time.monotonic()
         if blink_led:
